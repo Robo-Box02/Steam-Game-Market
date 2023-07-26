9b52316b86f65b0bd52b229e2546cfaee8adea84
Тагиев Самир Фуад <Tagie_zp84@itstep.edu.az>
2023-07-26 21:53:04 +0400

    Dragon

--- main.py
+++ main.py
@@ -3,13 +3,22 @@ from PIL import Image, ImageTk
 from tkinter.messagebox import *
 import json
 root = Tk()
+root.title("Steam")
 root.geometry('1920x1080')
+#Frame Menyu
 frame_menu = Frame(root,width = 1920,height = 1080,bg = '#0f1948')
 frame_menu.place(x=1,y=1)
+#Frame Login
 frame_login = Frame(root,width=1920,height=1080)
 frame_login.config(background="#0f1948")
+#Frame Registratiom
 frame_registration = Frame(root,width = 1920,height = 1080,bg = '#0f1948')
 frame_registration.config(background="#0f1948")
+#Frame Account Info
+frame_acc_info = Frame(root,width=1920,height=1080,bg="#0f1948")
+frame_acc_info.config(background="#0f1948")
+
+
 
 
 def main_menu():
@@ -23,6 +32,29 @@ def Registration():
     frame_registration.place(relx=0,rely=0)
 
 
+def back_to_menyu():
+    with open("Files/son.json","r")as FileHandler:
+        users = json.loads(FileHandler.readline())
+    for i in users:
+        if i ["login"] == entry1.get() and i["password"] == entry2.get():
+            frame_login.place_forget()
+            frame_menu.place(relx=0, rely=0)
+            user={
+                "login": i["login"],
+                "password": i ["password"],
+                "gmail": i ["gmail"]
+            }
+            acc_open(user)
+def acc_open(user):
+
+
+    Login_lbl = Label(frame_menu,text =user["login"],font= "Arial 14 bold",bg="#0f1948",fg="White")
+    Login_lbl.place(relx=0.6,rely=0.15)
+
+
+
+
+
 def register():
     if "@gmail.com" in entry_G.get():
         print(entry_G.get())
@@ -32,7 +64,7 @@ def register():
         print(entry_P.get())
     else:
         showerror("Error","Колличество символов должно быть больше 8")
-    if len(entry_CP.get()) == len(entry_P.get()):
+    if entry_CP.get() == entry_P.get():
         print(entry_CP.get())
     else:
         showerror("Error", "Пароль не совпадают")
@@ -53,7 +85,7 @@ def register():
     frame_registration.place_forget()
     frame_login.place(relx=0,rely=0)
 
-
+#Registration Board
 lbl_G = Label(frame_registration,text = "GMAIL",font= "Arial 14 bold",bg="#0f1948",fg="Yellow")
 lbl_G.place(relx=0.440,rely=0.200)
 entry_G = Entry(frame_registration,font="Arial 14 bold")
@@ -101,6 +133,8 @@ def Login():
         showerror("Error","Пороль введен не правельно, пожалуйста повторите попытку")
 
 
+
+#Вход в Магазин
 lbl1 = Label(frame_login,text="Login",font="Arial 14 bold",bg="#0f1948",fg="Yellow")
 lbl1.place(relx=0.480,rely=0.390)
 entry1 = Entry(frame_login,font= "Arial 14")
@@ -112,17 +146,17 @@ lbl2.place(relx=0.470,rely=0.440)
 entry2 = Entry(frame_login,font= "Arial 14")
 entry2.place(relx=0.455,rely=0.465,height=20)
 entry2.config(width=15)
-
+# Вход в мазазин
 btn1 = Button(frame_login,text="Registration",font="Arial 12 bold",bg="Yellow",command=Registration)
 btn1.place(relx=0.455,rely=0.490)
-btn2 = Button(frame_login,text = "Enter",font="Arial 12 bold",bg= "Yellow",command= Login)
+btn2 = Button(frame_login,text = "Enter",font="Arial 12 bold",bg= "Yellow",command=back_to_menyu)
 btn2.place(relx=0.515,rely=0.490)
 
 int_var = IntVar()
 check_btn = Checkbutton(frame_login, text='Switch states', variable=int_var, command=switch, font="Arial 10 bold", width=10,fg="Black")
 check_btn.place(relx=0.470, rely=0.530)
 
-
+# Список с играми
 img1 = Image.open("Photo/steam-icon-2048x2048-rbyixh0f.png")
 resized_image = img1.resize((150,100))
 photo = ImageTk.PhotoImage(resized_image)
@@ -160,9 +194,12 @@ label4=Label(frame_menu, image=photo4)
 label4.image=photo4
 label4.place(relx=0.184, rely=0.590)
 
-Bt_S= Button(frame_menu,text = "Shop",font="Arial 12 bold",borderwidth=4).place(relx=0.35,rely =0.030)
-Btn_L = Button(frame_menu,text="Library",font="Arial 12 bold",borderwidth=4).place(relx=0.45,rely=0.030)
-Btn_A = Button(frame_menu,text="Account",font="Arial 12 bold",borderwidth=4,command=enter_account).place(relx=0.55,rely=0.030)
-Btn_E = Button(frame_menu,text="Exit",font="Arial 12 bold",borderwidth=4).place(relx=0.65,rely=0.030)
+# Виджеты для переходов страниц
+Bt_Shop= Button(frame_menu,text = "Shop",font="Arial 12 bold",borderwidth=4).place(relx=0.35,rely =0.030)
+Btn_Library = Button(frame_menu,text="Library",font="Arial 12 bold",borderwidth=4).place(relx=0.45,rely=0.030)
+Btn_Account = Button(frame_menu,text="Account",font="Arial 12 bold",borderwidth=4,command=enter_account).place(relx=0.55,rely=0.030)
+Btn_Exit = Button(frame_menu,text="Exit",font="Arial 12 bold",borderwidth=4).place(relx=0.65,rely=0.030)
 ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 root.mainloop()
+
+isLogined = False
